a4fe1310cab9454b1ac49f6dda583cefa043925f
Angeline A. Banta <angelinebanta@gmail.com>
2026-05-21 15:00:48 +0800

    Create Lab17.php

---
+++ Lab17.php
@@ -0,0 +1,15 @@
+<form method="post" action="">
+    <label>Name:</label>
+    <input type="text" name="name" required>
+    <br><br>
+
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    <br><br>
+
+    <label>Age:</label>
+    <input type="number" name="age" required>
+    <br><br>
+
+    <input type="submit" value="Submit">
+</form>
